b46995a5f500c852973de21c83803fccdfe89f0e
SantiGuti <santiagogutierrez1905@gmail.com>
2020-11-08 15:50:09 -0300

    ordenamiento de codigo

--- src/Interfaz/Screen.java
+++ src/Interfaz/Screen.java
@@ -5,7 +5,6 @@ import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,25 +13,11 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import javax.swing.tree.DefaultMutableTreeNode;
-
 import codigoNegocio.GenerarArbolPersona;
 import codigoNegocio.Persona;
-import javax.swing.JTextArea;
 import java.awt.Font;
-import javax.swing.JList;
 import javax.swing.JTree;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import javax.swing.JSlider;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import javax.swing.JInternalFrame;
-import javax.swing.JSplitPane;
-import javax.swing.event.TreeExpansionListener;
-import javax.swing.event.TreeExpansionEvent;
 
 public class Screen extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
@@ -47,6 +32,12 @@ public class Screen extends JFrame implements ActionListener {
 	JButton backToMenuFromData = new JButton("\u2190");
 	JButton agregar = new JButton("Agregar persona");
 	JButton stats = new JButton("Estadisticas");
+	
+	Font texto12 = new Font("Microsoft Sans Serif", Font.PLAIN, 12);
+	Font texto20 = new Font("Microsoft Sans Serif", Font.PLAIN, 20);
+	Font texto30 = new Font("Microsoft Sans Serif", Font.PLAIN, 30);
+	Font botones = new Font("Segoe UI Symbol", Font.BOLD, 20);
+	
 	
 	CardLayout layout = new CardLayout();
 
@@ -96,151 +87,147 @@ public class Screen extends JFrame implements ActionListener {
 	}
 
 	private void addButtons() {
-		mainMenu.setFont(new Font("Segoe UI Symbol", Font.BOLD, 20));
+		// Botones agregados a diferentes layouts
+		mainMenu.setFont(botones);
 		mainMenu.setBounds(10, 10, 55, 31);
 		mainMenu.addActionListener(this);
-		backToMenuFromData.setFont(new Font("Segoe UI Symbol", Font.BOLD, 20));
-
+		game.add(mainMenu);
+		
+		backToMenuFromData.setFont(botones);
 		backToMenuFromData.setBounds(10, 10, 55, 31);
 		backToMenuFromData.addActionListener(this);
-		addData.setLayout(null);
-		game.setLayout(null);
-		estadisticas.setLayout(null);
-
-		// game buttons
-		game.add(mainMenu);
 		addData.add(backToMenuFromData);
-
-		// background colors
-		game.setBackground(Color.gray);
-		menu.setBackground(Color.DARK_GRAY);
-		addData.setBackground(Color.orange);
-		estadisticas.setBackground(Color.GRAY);
-		calculate.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 12));
-
-		// adding children to parent Panel
+		
+		calculate.setFont(texto12);
 		calculate.setBounds(300, 150, 180, 25);
 		calculate.addActionListener(this);
-		
-		menu.setLayout(null);
 		menu.add(calculate);
-		agregarDatos.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 12));
-		menu.add(agregarDatos);
 		
+		agregarDatos.setFont(texto12);
 		agregarDatos.setBounds(300, 300, 180, 25);
 		agregarDatos.addActionListener(this);
+		menu.add(agregarDatos);
 		
+		backToGameFromStats.setFont(new Font("Segoe UI Symbol", Font.BOLD, 20));
+		backToGameFromStats.setBounds(10, 10, 55, 31);
+		backToGameFromStats.addActionListener(this);
+		estadisticas.add(backToGameFromStats);
 		
+		// Setteo de los diferentes layouts
+		addData.setLayout(null);
+		addData.setBackground(Color.orange);
+		
+		game.setLayout(null);
+		game.setBackground(Color.gray);
+		
+		estadisticas.setLayout(null);
+		estadisticas.setBackground(Color.GRAY);
+		
+		menu.setLayout(null);
+		menu.setBackground(Color.DARK_GRAY);
+
 		panel.add(menu, "Menu");
 		panel.add(game, "Game");
 		panel.add(estadisticas, "Estadisticas");
-		backToGameFromStats.addActionListener(this);
-		backToGameFromStats.setFont(new Font("Segoe UI Symbol", Font.BOLD, 20));
-		backToGameFromStats.setBounds(10, 10, 55, 31);
+		panel.add(addData, "Personas");
+		getContentPane().add(panel);
+		layout.show(panel, "Menu");
 		
-		estadisticas.add(backToGameFromStats);
-		lblSimilaridadA.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 12));
-		lblSimilaridadA.setBounds(15, 490, 243, 68);
 		
+		// Monton de labels, trees, spinners que se agregan a diferentes layouts
+		lblSimilaridadA.setFont(texto12);
+		lblSimilaridadA.setBounds(15, 490, 243, 68);		
 		estadisticas.add(lblSimilaridadA);
-		lblstats.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 30));
+		
+		lblstats.setFont(texto30);
 		lblstats.setBounds(300, 34, 323, 105);
-		
 		estadisticas.add(lblstats);
-		lblSimilaridadB.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 12));
+		
+		lblSimilaridadB.setFont(texto12);
 		lblSimilaridadB.setBounds(410, 490, 243, 68);
-		
 		estadisticas.add(lblSimilaridadB);
-	//	estadisticas.add(mainMenu, BorderLayout.NORTH);
 		
-		treeB.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 19));
+		treeB.setFont(texto20);
 		treeB.setBackground(Color.GRAY);
 		treeB.setBounds(522, 63, 264, 447);
 		game.add(treeB);
 		
-		treeA.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 19));
+		treeA.setFont(texto20);
 		treeA.setBackground(Color.GRAY);
 		treeA.setBounds(26, 63, 264, 447);
 		game.add(treeA);
+		
 		stats.addActionListener(this);
 		stats.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 15));
 		stats.setBounds(300, 491, 213, 45);
 		game.add(stats);
 		
-		panel.add(addData, "Personas");
-		lblNames.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 12));
-
 		lblNames.setBounds(50, 92, 135, 15);
+		lblNames.setFont(texto12);
 		addData.add(lblNames);
-		lblDeportes.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 12));
 
 		lblDeportes.setBounds(50, 200, 135, 15);
+		lblDeportes.setFont(texto12);
 		addData.add(lblDeportes);
-		lblMusica.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 12));
 
 		lblMusica.setBounds(50, 270, 135, 15);
+		lblMusica.setFont(texto12);
 		addData.add(lblMusica);
-		lblEspectac.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 12));
-
+		
 		lblEspectac.setBounds(50, 340, 135, 15);
+		lblEspectac.setFont(texto12);
 		addData.add(lblEspectac);
-		lblCiencia.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 12));
 
 		lblCiencia.setBounds(50, 410, 135, 15);
+		lblCiencia.setFont(texto12);
 		addData.add(lblCiencia);
 
 		name.setBounds(113, 90, 487, 20);
 		addData.add(name);
 
-		addData.add(deportes);
 		deportes.setModel(new SpinnerNumberModel(1, 1, 5, 1));
 		deportes.setBounds(200, 195, 30, 20);
+		addData.add(deportes);
 
-		addData.add(musica);
 		musica.setModel(new SpinnerNumberModel(1, 1, 5, 1));
 		musica.setBounds(200, 265, 30, 20);
+		addData.add(musica);
 
-		addData.add(espectaculos);
 		espectaculos.setModel(new SpinnerNumberModel(1, 1, 5, 1));
 		espectaculos.setBounds(200, 335, 30, 20);
-
-		addData.add(ciencia);
+		addData.add(espectaculos);
+		
 		ciencia.setModel(new SpinnerNumberModel(1, 1, 5, 1));
 		ciencia.setBounds(200, 405, 30, 20);
-		agregar.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 12));
-
-		addData.add(agregar);
-		agregar.setBounds(300, 500, 180, 25);
-		lblIntereses.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 16));
-		lblIntereses.setBounds(50, 146, 100, 31);
+		addData.add(ciencia);
 		
+		lblIntereses.setFont(texto20);
+		lblIntereses.setBounds(50, 146, 100, 31);
 		addData.add(lblIntereses);
+		
+		agregar.setBounds(300, 500, 180, 25);
+		agregar.setFont(texto12);
+		addData.add(agregar);
 		agregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				int valor = JOptionPane.showConfirmDialog(null, "Desea cargar la informacion?", "Confirmacion",	JOptionPane.YES_NO_OPTION);
-				if (valor == JOptionPane.YES_OPTION) {
-					if (name.getText().isEmpty()){
-						// Si el usuario no ingreso nombre, le avisa
-						JOptionPane.showMessageDialog(null, "Ingrese el nombre de la persona");
-					} else {
-						// Si esta todo ok agrego a la persona
-						
-						boolean noExiste = arbol.crearYAgregarPersona(name.getText(), (int) deportes.getValue(), (int) musica.getValue(), (int) espectaculos.getValue(), (int) ciencia.getValue());
-						if(!noExiste) JOptionPane.showMessageDialog(null, "Esa persona ya existe!");
-						// Reinicio los valores de los spinners y del name
-						name.setText("");
-						deportes.setValue(1);
-						musica.setValue(1);
-						espectaculos.setValue(1);
-						ciencia.setValue(1);
-					}
+				
+				if (name.getText().isEmpty()) {
+					// Si el usuario no ingreso nombre, le avisa y no agrega a la persona
+					JOptionPane.showMessageDialog(null, "Ingrese el nombre de la persona");
+				}
+				 else {
+					// Si esta todo ok agrego a la persona
+					boolean noExiste = arbol.crearYAgregarPersona(name.getText(), (int) deportes.getValue(), (int) musica.getValue(), (int) espectaculos.getValue(), (int) ciencia.getValue());
+					if(!noExiste) JOptionPane.showMessageDialog(null, "Esa persona ya existe!");
+					// Reinicio los valores de los spinners y del name
+					name.setText("");
+					deportes.setValue(1);
+					musica.setValue(1);
+					espectaculos.setValue(1);
+					ciencia.setValue(1);
 				}
 			}
 		});
-
-		getContentPane().add(panel);
-		layout.show(panel, "Menu");
-
 	}
 
 	private void expandAllNodes(JTree tree) {
